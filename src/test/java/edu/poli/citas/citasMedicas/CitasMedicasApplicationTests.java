3c240c77cd249ad8b5151cc9264354eacee2259a
Mario Alejandro Mendez <marioalejandro.mendez.contractor@bbva.com>
2024-04-15 11:14:40 -0500

    comit inicial

---
+++ src/test/java/edu/poli/citas/citasMedicas/CitasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.poli.citas.citasMedicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
