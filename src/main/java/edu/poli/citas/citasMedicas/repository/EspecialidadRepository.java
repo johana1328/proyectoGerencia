00c7cab4da39907e24181dfdbb58dd1c0b025c7a
Mario Alejandro Mendez <marioalejandro.mendez.contractor@bbva.com>
2024-04-16 09:27:56 -0500

    init repositorios

---
+++ src/main/java/edu/poli/citas/citasMedicas/repository/EspecialidadRepository.java
@@ -0,0 +1,9 @@
+package edu.poli.citas.citasMedicas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import edu.poli.citas.citasMedicas.model.EspecialidadModel;
+
+public interface EspecialidadRepository extends JpaRepository<EspecialidadModel, Long> {
+
+}
