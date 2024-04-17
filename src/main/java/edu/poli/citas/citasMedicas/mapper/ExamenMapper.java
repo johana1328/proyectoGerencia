f6487a20a058a7098e622389b1d68919d67a9439
johana1328 <johanakatherin05@gmail.com>
2024-04-17 11:59:51 -0500

    Nivelacion Service

--- src/main/java/edu/poli/citas/citasMedicas/mapper/ExamenMapper.java
+++ src/main/java/edu/poli/citas/citasMedicas/mapper/ExamenMapper.java
@@ -2,18 +2,27 @@ package edu.poli.citas.citasMedicas.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import edu.poli.citas.citasMedicas.dto.ExamenDto;
+import edu.poli.citas.citasMedicas.model.EspecialidadModel;
 import edu.poli.citas.citasMedicas.model.ExamenModel;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public abstract class ExamenMapper {
 	
-	public abstract ExamenDto toDto(ExamenModel source);
+	@Mapping(target = "autorizacion", ignore = true)
+	public abstract ExamenDto toDto(ExamenModel model);
 
 	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "autorizacion", ignore = true)
 	public abstract ExamenModel toModel(ExamenDto source);
+	
+	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "autorizacion", ignore = true)
+	public abstract ExamenModel merge(@MappingTarget  ExamenModel model, ExamenDto dto);
+
 
 
 }
--- src/main/java/edu/poli/citas/citasMedicas/mapper/MedicamentoMapper.java
+++ src/main/java/edu/poli/citas/citasMedicas/mapper/MedicamentoMapper.java
@@ -2,6 +2,7 @@ package edu.poli.citas.citasMedicas.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import edu.poli.citas.citasMedicas.dto.MedicamentoDto;
@@ -10,9 +11,15 @@ import edu.poli.citas.citasMedicas.model.MedicamentoModel;
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public abstract class MedicamentoMapper {
 	
-	public abstract MedicamentoDto toDto(MedicamentoModel source);
+	@Mapping(target = "autorizacion", ignore = true)
+	public abstract MedicamentoDto toDto(MedicamentoModel model);
 
 	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "autorizacion", ignore = true)
 	public abstract MedicamentoModel toModel(MedicamentoDto source);
+	
+	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "autorizacion", ignore = true)
+	public abstract MedicamentoModel merge(@MappingTarget  MedicamentoModel model, MedicamentoDto dto);
 
 }
