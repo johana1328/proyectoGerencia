90b39a2cf3718787fbd6a9afb077d1d602a4e72c
johana1328 <johanakatherin05@gmail.com>
2024-04-21 11:58:52 -0500

    ajuste login

--- src/main/java/edu/poli/citas/citasMedicas/controller/LoginController.java
+++ src/main/java/edu/poli/citas/citasMedicas/controller/LoginController.java
@@ -2,10 +2,8 @@ package edu.poli.citas.citasMedicas.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/inicio")
 public class LoginController {
 
 	@GetMapping({ "", "/" })
