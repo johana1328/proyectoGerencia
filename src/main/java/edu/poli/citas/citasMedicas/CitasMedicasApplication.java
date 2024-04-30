28dcd1e56503941d36af14870e3bef785cb74071
Mario Alejandro Mendez <marioalejandro.mendez.contractor@bbva.com>
2024-04-30 01:23:26 -0500

    citas

--- src/main/java/edu/poli/citas/citasMedicas/CitasMedicasApplication.java
+++ src/main/java/edu/poli/citas/citasMedicas/CitasMedicasApplication.java
@@ -2,15 +2,21 @@ package edu.poli.citas.citasMedicas;
 
 import javax.sql.DataSource;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @Configuration
 public class CitasMedicasApplication {
+	
+	@Autowired
+	private Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CitasMedicasApplication.class, args);
@@ -19,7 +25,7 @@ public class CitasMedicasApplication {
 	@Bean
 	DataSource getDataSource() {
 		return DataSourceBuilder.create().driverClassName("org.postgresql.Driver")
-				.url("jdbc:postgresql://192.168.0.23:5432/citas_medicas").username("postgres").password("postgres").build();
+				.url(env.getProperty("datasource")).username("postgres").password("postgres").build();
 	}
 
 }
